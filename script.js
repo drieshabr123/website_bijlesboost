c0ab197d05ca660afdc6792e1ac83dbc078fb124
drieshabr123 <147876640+drieshabr123@users.noreply.github.com>
2024-11-05 14:05:48 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("contactButton").addEventListener("click", function() {
+    alert("Bedankt voor je interesse! Stuur ons een e-mail op info@bijlesboost.be om contact op te nemen.");
+});
